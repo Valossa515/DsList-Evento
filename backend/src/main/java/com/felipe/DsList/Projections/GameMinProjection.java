833c502cf71bb61f6d8ab7165a111d3747902d67
Valossa515 <fe.mmo515@gmail.com>
2023-07-12 09:24:26 -0300

    alterações em application.properties, repositorios e Projection

---
+++ backend/src/main/java/com/felipe/DsList/Projections/GameMinProjection.java
@@ -0,0 +1,10 @@
+package com.felipe.DsList.Projections;
+
+public interface GameMinProjection {
+	Long getId();
+	String getTitle();
+	Integer getGameYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+}
